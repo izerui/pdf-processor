6cb82f435a748eb725efd29c03612d0a67a72df6
serv <liuyuhua69@gmail.com>
2023-09-13 17:32:10 +0800

    1

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t pdf-processor:1.0 .
-docker tag pdf-processor:1.0 harbor.yj2025.com/library/pdf-processor:1.0
-docker push harbor.yj2025.com/library/pdf-processor:1.0
+docker build -f Dockerfile -t pdf-processor:1.1.
+docker tag pdf-processor:1.1 harbor.yj2025.com/library/pdf-processor:1.1
+docker push harbor.yj2025.com/library/pdf-processor:1.1
