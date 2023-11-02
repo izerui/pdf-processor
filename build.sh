6e009050818f546aca99742604965372f755c6b6
serv <liuyuhua69@gmail.com>
2023-11-02 17:04:30 +0800

    注意关闭文档

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t pdf-processor:1.1 .
-docker tag pdf-processor:1.1 harbor.yj2025.com/library/pdf-processor:1.1
-docker push harbor.yj2025.com/library/pdf-processor:1.1
+docker build -f Dockerfile -t pdf-processor:1.2 .
+docker tag pdf-processor:1.2 harbor.yj2025.com/library/pdf-processor:1.2
+docker push harbor.yj2025.com/library/pdf-processor:1.2
