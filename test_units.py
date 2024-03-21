bce41896cc36c8ca457dd136afeda7aca3aecd06
serv <liuyuhua69@gmail.com>
2024-03-21 11:56:02 +0800

    遮罩问题

--- test_units.py
+++ test_units.py
@@ -32,9 +32,8 @@ class TestTable(unittest.TestCase):
             with fitz.open(file_path) as doc:
                 for index, page in enumerate(doc):
                     page: Page = page
-                    # 遮罩页
+                    # 遮罩页, 使用源页面未旋转前的矩形区域
                     mark_page = mark_pdf.new_page(width=page.cropbox.width, height=page.cropbox.height)
-                    mark_page.set_rotation(page.rotation)
                     # 页面传递进来的缩放倍数,这里使用的时候要进行反向缩放，才能适配原始页面的坐标系
                     zoom = 1
                     # 矩形的左上、右下坐标数组 [x0,y0,x1,y1]
@@ -43,6 +42,30 @@ class TestTable(unittest.TestCase):
                         p.append(i / zoom)
 
                     rect = fitz.Rect(float(p[0]), float(p[1]), float(p[2]), float(p[3]))
+
+                    # 矩阵旋转 https://pymupdf.readthedocs.io/en/latest/page.html#Page.rotation_matrix
+                    # rect = rect.transform(page.transformation_matrix)
+                    # 等同于: rect = rect * page.transformation_matrix
+
+                    print(filename, f'旋转:【{page.rotation}】')
+                    print(f'    页面宽高:{page.bound()}')
+                    print(f'    插入坐标矩形:{rect}')
+                    print(f'    变换矩阵:{page.transformation_matrix}')
+                    print(f'    旋转矩阵:{page.rotation_matrix}')
+                    print(f'    反旋矩阵:{page.derotation_matrix}')
+
+                    # 获取可见页面大小
+                    media_box = page.mediabox
+                    # 获取缩放量和偏移量
+                    scale = media_box.width / media_box.height
+                    offset_x = media_box.x0
+                    offset_y = media_box.y0
+
+                    print("         缩放量：", scale)
+                    print("         偏移量 X：", offset_x)
+                    print("         偏移量 Y：", offset_y)
+                    print("         rect cropbox mediabox 是否一致：", page.rect == page.cropbox == page.mediabox)
+
                     # img_url = None
                     img_url = 'https://cdn.pixabay.com/photo/2023/11/09/19/36/zoo-8378189_1280.jpg'
                     if img_url:
@@ -62,10 +85,16 @@ class TestTable(unittest.TestCase):
                         shape.commit()
 
                     # page.show_pdf_page(mark_page.cropbox, mark_pdf, rotate=page.rotation, keep_proportion=True, clip=page.cropbox, overlay=True)
-                    mark_page.show_pdf_page(page.cropbox, doc, keep_proportion=True,
+                    rotation = page.rotation
+                    # 合并前设置两个页面旋转为0，保证页面一致
+                    page.set_rotation(0)
+                    mark_page.show_pdf_page(page.cropbox, doc, keep_proportion=True, rotate=0,
                                             clip=mark_page.cropbox, overlay=True)
-
-                mark_pdf.save(os.path.join(dir, f'__|{p[0]}|{p[1]}|{p[2]}|{p[3]}|_{filename}'))
-                mark_pdf.close()
+                    # 合并后页面恢复原来的旋转角度
+                    page.set_rotation(rotation)
+                    mark_page.set_rotation(page.rotation)
+                # doc.save(os.path.join(dir, f'__doc_|{p[0]}|{p[1]}|{p[2]}|{p[3]}|_{filename}'))
+            mark_pdf.save(os.path.join(dir, f'__mark_|{p[0]}|{p[1]}|{p[2]}|{p[3]}|_{filename}'))
+            mark_pdf.close()
 
     pass
