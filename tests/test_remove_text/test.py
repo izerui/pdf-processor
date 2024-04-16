8457d948575b54ed9b39f44b22da901559d4cca2
serv <liuyuhua69@gmail.com>
2024-04-16 18:17:12 +0800

    删除文本

--- tests/test_remove_text/test.py
+++ tests/test_remove_text/test.py
@@ -6,8 +6,17 @@ def test():
     page = doc[0]
     blocks = page.get_textpage().extractDICT()['blocks']
     for block in blocks:
-        print(block)
-        for line in block['lines']:
-            print(line)
-        pass
+        for line in block["lines"]:
+            for span in line['spans']:
+                if span['text'] == 'Copyright 2016-2024 Aspose Pty Ltd.':
+                    page.add_redact_annot(span["bbox"])
+                elif span['text'] == 'Created with Aspose.CAD.':
+                    page.add_redact_annot(span["bbox"])
+                elif span['text'] == 'Evaluation only.':
+                    page.add_redact_annot(span["bbox"])
+    page.apply_redactions(images=fitz.PDF_REDACT_IMAGE_NONE, graphics=fitz.PDF_REDACT_LINE_ART_NONE)  # remove text, but no image
+    # for xref in page.get_contents():
+    #     stream = doc.xref_stream(xref).replace(b'Copyright 2016-2024 Aspose Pty Ltd.', b'')
+    #     doc.update_stream(xref, stream)
+    doc.ez_save('x.pdf')
     pass
