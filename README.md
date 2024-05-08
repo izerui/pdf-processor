246acf041f759405208e1b6dbdd4d0c877ccda8f
serv <liuyuhua69@gmail.com>
2024-05-08 16:54:25 +0800

    修改支持 多模版3

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 
 第三方库对比: https://dothinking.github.io/2021-01-02-Python%E5%A4%84%E7%90%86PDF%E7%9A%84%E7%AC%AC%E4%B8%89%E6%96%B9%E5%BA%93%E5%AF%B9%E6%AF%94/
 
+```
+# 安装依赖
+pip install -r requirements.txt
+# 生成依赖描述文件
+pip freeze > requirements.txt
+```
+
 
 流程图: 
 
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t pdf-processor:1.4 .
-docker tag pdf-processor:1.4 harbor.yj2025.com/library/pdf-processor:1.4
-docker push harbor.yj2025.com/library/pdf-processor:1.4
+docker build -f Dockerfile -t pdf-processor:1.5 .
+docker tag pdf-processor:1.5 harbor.yj2025.com/library/pdf-processor:1.5
+docker push harbor.yj2025.com/library/pdf-processor:1.5
