7ec353087752e20cc676c31eb4eb715e071bf44c
serv <liuyuhua69@gmail.com>
2024-06-15 10:51:05 +0800

    return target_doc

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def callback_from_urls(callback_items: CallbackItems):
                 # 暂时不考虑上传结果接口异常,出现异常，由业务方重新调用即可。
                 logger.info(f'【开始上传结果文档到业务应用】: {repr(files)}')
                 response = httpx.post(callback_items.callback_url, files=files, data=data,
-                                      timeout=Timeout(timeout=300.0, connect=10.0))
+                                      timeout=Timeout(timeout=300.0, connect=30.0))
                 if response.is_success:
                     logger.info(f'【上传pdf返回结果】: {response.content}')
 
