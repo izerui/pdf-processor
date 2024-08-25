ebdef52bf9eb02414127aaf3589468d3037aaf69
serv <liuyuhua69@gmail.com>
2024-08-25 13:02:02 +0800

    升级 到 1.6， 原始区域问题

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t pdf-processor:1.5 .
-docker tag pdf-processor:1.5 harbor.yj2025.com/library/pdf-processor:1.5
-docker push harbor.yj2025.com/library/pdf-processor:1.5
+docker build -f Dockerfile -t pdf-processor:1.6 .
+docker tag pdf-processor:1.6 harbor.yj2025.com/library/pdf-processor:1.6
+docker push harbor.yj2025.com/library/pdf-processor:1.6
