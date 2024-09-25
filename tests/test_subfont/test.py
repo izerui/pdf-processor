d683e3c318cb079697ff62ab24ded4792e5c1221
serv <liuyuhua69@gmail.com>
2024-09-25 15:07:57 +0800

    551

--- tests/test_subfont/test.py
+++ tests/test_subfont/test.py
@@ -1,13 +1,24 @@
 import pymupdf
 
 
+def test_subfont0():
+    doc = pymupdf.open()
+    page = doc.new_page(width=1024, height=768)
+    page.insert_htmlbox(
+        rect=pymupdf.Rect(280, 50, 480, 100),
+        text=f'<span style="font-size:18px;font-weight:bold;word-break:break-all;font-family:\'helv\';">Α、Β、Γ、Δ、Ε、Ζ、Η、Θ、Ι、Κ、Λ、Μ、Ν、Ξ、Ο、Π、Ρ、Σ、Τ、Υ、Φ、Χ、Ψ、Ω。α、β、γ、δ、ε、ζ、η、θ、ι、κ、λ、μ、ν、ξ、ο、π、ρ、ς、τ、υ、φ、χ、ψ、ω。</span>'
+    )
+    doc.subset_fonts()
+    doc.ez_save('subseted.pdf')
+
+
 def test_subfont1():
     chn_fontname = 'chn'
     with open('FangZhengHeiTiJianTi-1.ttf', 'rb') as f:
         font_buffer = bytes(f.read())
     font = pymupdf.Font(fontname=chn_fontname,
-                     fontbuffer=font_buffer,
-                     language='zh-Hans')
+                        fontbuffer=font_buffer,
+                        language='zh-Hans')
     doc = pymupdf.open()
     page = doc.new_page(width=1024, height=768)
     page.insert_font(fontname=chn_fontname,
