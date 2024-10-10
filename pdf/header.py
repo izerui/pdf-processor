d725645ce326ec0fe9a1c765000a759c174bba82
serv <liuyuhua69@gmail.com>
2024-10-10 15:53:38 +0800

    二维码宽度可自定义qr_code_size top_padding

--- pdf/header.py
+++ pdf/header.py
@@ -62,7 +62,10 @@ class IHeader(object):
             top_padding = 0
             left_padding = 80
             if self.item.qr_code_size < 180:
-                top_padding = (180 - self.item.qr_code_size) / 4
+                if self.item.header_layout == 'top':
+                    top_padding = (180 - self.item.qr_code_size) * 3/5
+                elif self.item.header_layout == 'bottom':
+                    top_padding = (180 - self.item.qr_code_size) * 2/5
             x0 = left_padding + self.item.header_padding_left
             y0 = top_padding
             x1 = left_padding + self.item.header_padding_left + self.item.qr_code_size
